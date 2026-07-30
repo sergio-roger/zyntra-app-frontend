bf4b8b793faa6ca5f25e267f357b37acdd2432b4
Sergio Roger <sergio.lkg.31@gmail.com>
2026-07-30 01:08:42 -0500

    feat(content-planning): add ContentPlanCalendarView using FullCalendar

---
+++ src/features/content-planning/components/ContentPlanCalendarView.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { ContentPost } from '@features/content-planning/interfaces/content-post.interface';
+import { buildCalendarEvents } from '@features/content-planning/utils/build-calendar-events';
+
+interface ContentPlanCalendarViewProps {
+  posts: ContentPost[];
+  onDayClick: (date: string) => void;
+}
+
+export const ContentPlanCalendarView: React.FC<ContentPlanCalendarViewProps> = ({ posts, onDayClick }) => (
+  <FullCalendar
+    plugins={[dayGridPlugin, interactionPlugin]}
+    initialView="dayGridMonth"
+    events={buildCalendarEvents(posts)}
+    dateClick={(info) => onDayClick(info.dateStr)}
+    eventClick={(info) => onDayClick(info.event.startStr.slice(0, 10))}
+    height="auto"
+  />
+);
+
+export default ContentPlanCalendarView;
